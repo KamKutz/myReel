c592802df63ef1cb046d73699a10e364b946994b
KamKutz <KineticK@Kamerons-MacBook-Pro-2.local>
2017-11-21 12:04:16 -0500

    Project-push 2

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ $("#genre_button").click(function(event) {
 });
 
 
+
 // Toggle Rating System for Stars
 
 $("#starBtn_1").click(function(event) {
@@ -82,6 +83,19 @@ $("#horrorBtn_1").click(function(event) {
     }
 });
 
+$('#horrorBtnG_1').click(function(event) {
+    // stop event from bubbling up to genre-active element
+    event.stopPropagation();
+
+    $("#horrorBtnG_1").toggleClass("horror_selected");
+
+   if($(this).hasClass('horror_selected')){
+        $(this).removeClass('horror');
+    } else {
+      $(this).addClass('horror');
+    }
+});
+
 
 $("#comedyBtn_2").click(function(event) {
     $("#comedyBtn_2").toggleClass("comedy_b_selected");
@@ -90,6 +104,17 @@ $("#comedyBtn_2").click(function(event) {
         $(this).removeClass('comedy_b');
     } else {
       $(this).addClass('comedy_b');
+    }
+});
+
+$("#comedyBtnG_2").click(function(event) {
+    event.stopPropagation();
+    $("#comedyBtnG_2").toggleClass("comedy_selected");
+
+   if($(this).hasClass('comedy_selected')){
+        $(this).removeClass('comedy');
+    } else {
+      $(this).addClass('comedy');
     }
 });
 
@@ -104,6 +129,17 @@ $("#adventureBtn_3").click(function(event) {
     }
 });
 
+$("#adventureBtnG_3").click(function(event) {
+    event.stopPropagation();
+    $("#adventureBtnG_3").toggleClass("adventure_selected");
+
+   if($(this).hasClass('adventure_selected')){
+        $(this).removeClass('adventure');
+    } else {
+      $(this).addClass('adventure');
+    }
+});
+
 
 $("#actionBtn_4").click(function(event) {
     $("#actionBtn_4").toggleClass("action_b_selected");
@@ -112,6 +148,17 @@ $("#actionBtn_4").click(function(event) {
         $(this).removeClass('action_b');
     } else {
       $(this).addClass('action_b');
+    }
+});
+
+$("#actionBtnG_4").click(function(event) {
+    event.stopPropagation();
+    $("#actionBtnG_4").toggleClass("action_selected");
+
+   if($(this).hasClass('action_selected')){
+        $(this).removeClass('action');
+    } else {
+      $(this).addClass('action');
     }
 });
 
@@ -126,6 +173,17 @@ $("#romanceBtn_5").click(function(event) {
     }
 });
 
+$("#romanceBtnG_5").click(function(event) {
+    event.stopPropagation();
+    $("#romanceBtnG_5").toggleClass("romance_selected");
+
+   if($(this).hasClass('romance_selected')){
+        $(this).removeClass('romance');
+    } else {
+      $(this).addClass('romance');
+    }
+});
+
 
 $("#dramaBtn_6").click(function(event) {
     $("#dramaBtn_6").toggleClass("drama_b_selected");
@@ -134,6 +192,17 @@ $("#dramaBtn_6").click(function(event) {
         $(this).removeClass('drama_b');
     } else {
       $(this).addClass('drama_b');
+    }
+});
+
+$("#dramaBtnG_6").click(function(event) {
+    event.stopPropagation();
+    $("#dramaBtnG_6").toggleClass("drama_selected");
+
+   if($(this).hasClass('drama_selected')){
+        $(this).removeClass('drama');
+    } else {
+      $(this).addClass('drama');
     }
 });
 
@@ -148,6 +217,17 @@ $("#fantasyBtn_7").click(function(event) {
     }
 });
 
+$("#fantasyBtnG_7").click(function(event) {
+    event.stopPropagation();
+    $("#fantasyBtnG_7").toggleClass("fantasy_selected");
+
+   if($(this).hasClass('fantasy_selected')){
+        $(this).removeClass('fantasy');
+    } else {
+      $(this).addClass('fantasy');
+    }
+});
+
 
 $("#cartoonBtn_8").click(function(event) {
     $("#cartoonBtn_8").toggleClass("cartoon_b_selected");
@@ -159,6 +239,17 @@ $("#cartoonBtn_8").click(function(event) {
     }
 });
 
+$("#cartoonBtnG_8").click(function(event) {
+    event.stopPropagation();
+    $("#cartoonBtnG_8").toggleClass("cartoon_selected");
+
+   if($(this).hasClass('cartoon_selected')){
+        $(this).removeClass('cartoon');
+    } else {
+      $(this).addClass('cartoon');
+    }
+});
+
 
 $("#sci-fiBtn_9").click(function(event) {
     $("#sci-fiBtn_9").toggleClass("sci-fi_b_selected");
@@ -167,6 +258,17 @@ $("#sci-fiBtn_9").click(function(event) {
         $(this).removeClass('sci-fi_b');
     } else {
       $(this).addClass('sci-fi_b');
+    }
+});
+
+$("#sci-fiBtnG_9").click(function(event) {
+    event.stopPropagation();
+    $("#sci-fiBtnG_9").toggleClass("sci-fi_selected");
+
+   if($(this).hasClass('sci-fi_selected')){
+        $(this).removeClass('sci-fi');
+    } else {
+      $(this).addClass('sci-fi');
     }
 });
 
@@ -203,22 +305,72 @@ window.onclick = function(event) {
 
 
 
+
+
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById('theModalView');
+
+// Get the button that opens the modal
+var movie = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    $(".modal").addClass("show");
+    $(".modal").removeClass("disappear");
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    $(".modal").addClass("disappear");
+    $(".modal").removeClass("show");
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // LOCAL STORAGE FUNCTIONALITY STARTS
 
-$(window).scroll(function(){
-    $(".movie_container").css("opacity", 1 - $(window).scrollTop() / 250);
-  });
+// $(window).scroll(function(){
+//     $(".movie_container").css("opacity", 1 - $(window).scrollTop() / 250);
+//   });
 
 
 
 
 $(function() {
 $('.confirm').click(function(){
-    var newMovie = $('<div id="movie_btn" class="movie"> <h4>Movie Title</h4> </div>');
-  //newDiv.style.background = "#000";
+    var newMovie = $('<div class="movie"> <h4>Movie Title</h4> </div>');
+
   $('.movie_container').append(newMovie);
   $(".modal").addClass("disappear");
   $(".modal").removeClass("show");
+
+  $(".temp_main").addClass("remove_temp-main");
 
 });
 });
